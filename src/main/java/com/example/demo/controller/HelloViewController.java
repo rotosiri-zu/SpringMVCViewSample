fe69cce8295b1a0f0e679ce86a144bf5ba4c275a
rotosiri-zu <hongjitufang25@gmail.com>
2024-12-12 13:54:44 +0900

    コントローラの修正

--- src/main/java/com/example/demo/controller/HelloViewController.java
+++ src/main/java/com/example/demo/controller/HelloViewController.java
@@ -1,15 +1,17 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("hello")
 public class HelloViewController {
-
-	@GetMapping("view")
-	public String helloview() {
+	@GetMapping("model")
+	public String helloview(Model model) {
+		// [Model]にデータを格納する
+		model.addAttribute("msg", "タイムリーフ!!!");		
 		// 戻り値は「ビュー名」を返す
 		return "hello";
 	}
