73039a30ecf4b9e051878545b2250170d559056e
rotosiri-zu <hongjitufang25@gmail.com>
2024-12-14 14:18:29 +0900

    ModelAndViewの使用

--- src/main/java/com/example/demo/controller/HelloViewController.java
+++ src/main/java/com/example/demo/controller/HelloViewController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("hello")
@@ -13,6 +14,14 @@ public class HelloViewController {
 		// [Model]にデータを格納する
 		model.addAttribute("msg", "タイムリーフ!!!");		
 		// 戻り値は「ビュー名」を返す
-		return "hello";
+		return "helloThymeleaf";
+	}
+	@GetMapping("modelandview")
+	public ModelAndView helloView2(ModelAndView modelAndView) {
+		// データを格納する
+		modelAndView.addObject("msg", "タイムリーフ!!!");
+		// 画面を設定する
+		modelAndView.setViewName("helloThymeleaf");
+		return modelAndView;
 	}
 }
